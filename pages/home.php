2fa0ee92095055461e24f81723146c4bf6324904
Irene Borri <irene.borri@studio.unibo.it>
2026-05-26 17:13:54 +0200

    fix: database name in bootstrap.php + add requirement of bootstrap.php in home.php

--- pages/home.php
+++ pages/home.php
@@ -1,13 +1,15 @@
 <?php
 
+require_once '../bootstrap.php';
+
 if (isUserLoggedIn()) {
-    $TemplateParams["page"] = "pages/home_loggato.php";
+    $templateParams["page"] = "home_loggato.php";
 } else {   
-    $templateParams["page"] = "pages/home_pubblica.php";
+    $templateParams["page"] = "home_pubblica.php";
     $templateParams["gruppiStudio"] = $dbh->getGruppiPerTipo('studio',8);
     $templateParams["gruppiProgetto"] = $dbh->getGruppiPerTipo('progetto',8);
     $templateParams["gruppiCasuali"] = $dbh->getGruppiCasuali(8);
 }
 
-require "pages/templates/base.php"
+require "templates/base.php"
 ?>

8bf5fb2fcd7a30ec71650b5989fcc87bfbdaf8f0
Irene Borri <irene.borri@studio.unibo.it>
2026-05-28 18:07:01 +0200

    fix: remove boostrap and add css for courses page

--- pages/templates/specific/template-corsi.php
+++ pages/templates/specific/template-corsi.php
@@ -1,15 +1,15 @@
-<main class="container mt-5 mb-10">
-        <section class="container p-0">
-            <ul class="row row-cols-md-4 row-cols-xs-1 p-0 g-2">
+    <main class="courses-main">
+        <section class="courses-container">
+            <ul class="courses-list">
                 <?php foreach($templateParams["corsi"] as $corso):?>
-                <li class="col border border-info border-3 rounded m-0 p-2">
-                    <h2 class="fs-4 text-info"><?php echo $corso["Nome"]; ?></h2>
+                <li class="course">
+                    <h2><?php echo $corso["Nome"]; ?></h2>
                     <p>Codice : <?php echo $corso["Codice"]; ?></p>
                     <p>Crediti : <?php echo $corso["CFU"]; ?></p>
                     <p>Richiede progetto : <?php echo $corso["ProgettoRichiesto"]; ?></p>
                     <p>Descrizione : <?php echo $corso["Descrizione"]; ?></p>
-                    <div class="text-end">
-                        <a class="btn btn-info" href="">Vai ai gruppi</a>
+                    <div>
+                        <a href="">Vai ai gruppi</a>
                     </div>
                 </li>
                 <?php endforeach ?>
